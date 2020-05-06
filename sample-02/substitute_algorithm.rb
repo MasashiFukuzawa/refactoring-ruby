230a1d3e79114db49ded71e14fb59208908e297c
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-05-06 18:15:39 +0900

    Substitute algorithm

---
+++ sample-02/substitute_algorithm.rb
@@ -0,0 +1,23 @@
+# Before
+def found_friends(people)
+  friends = []
+  people.each do |person|
+    if person == 'Don'
+      friends << 'Don'
+    end
+    if person == 'John'
+      friends << 'John'
+    end
+    if person == 'Kent'
+      friends << 'Kent'
+    end
+  end
+  friends
+end
+
+# After
+def found_friends(people)
+  people.select do |person|
+    %w(Don John Kent).include?(person)
+  end
+end
