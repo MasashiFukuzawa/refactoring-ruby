8eb4267145708c44a38dd8efe94a9b956c70e04b
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-05-06 21:49:37 +0900

    Move method

---
+++ sample-03/move_method.rb
@@ -0,0 +1,45 @@
+# Before
+class Account
+  def overdraft_charge
+    if @account_type.premium?
+      result = 10
+      result += (@days_overdrawn - 7) * 0.85 if @days_overdrawn > 7
+      result
+    else
+      @days_overdrawn * 1.75
+    end
+  end
+
+  def back_charge
+    result = 4.5
+    result += overdraft_charge if @days_overdrawn > 0
+    result
+  end
+end
+
+# After
+class AccountType
+  def overdraft_charge(days_overdrawn)
+    if premium?
+      result = 10
+      result += (days_overdrawn - 7) * 0.85 if days_overdrawn > 7
+      result
+    else
+      days_overdrawn * 1.75
+    end
+  end
+end
+
+class Account
+  def overdraft_charge
+    @account_type.overdraft_charge(@days_overdrawn)
+  end
+
+  def back_charge
+    result = 4.5
+    if @days_overdrawn > 0
+      result += @account_type.overdraft_charge(@days_overdrawn)
+    end
+    result
+  end
+end
