deda764d4ba73110fec215054f61fbec19d1c6f3
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-05-06 19:28:35 +0900

    Remove unused default parameters

---
+++ sample-02/remove_unused_default_parameter.rb
@@ -0,0 +1,11 @@
+# Before
+def product_count_items(search_criteria = nil)
+  criteria = search_criteria || @search_criteria
+  ProductCountItem.find_all_by_criteria(criteria)
+end
+
+# After
+def product_count_items(search_criteria)
+  criteria = search_criteria || @search_criteria
+  ProductCountItem.find_all_by_criteria(criteria)
+end
