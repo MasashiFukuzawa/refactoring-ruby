3e5e058705124060db0a8a0d3ee8f5103e4d71ff
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-05-06 18:55:06 +0900

    Introduce class annotation

---
+++ sample-02/introduce_class_annotation.rb
@@ -0,0 +1,26 @@
+# Before
+class SearchCriteria
+  def initialize(hash)
+    @author_id = hash[:author_id]
+    @publisher_id = hash[:publisher_id]
+    @isbn = hash[:isbn]
+  end
+end
+
+# After
+class SearchCriteria
+  hash_initializer :author_id, :publisher_id, :isbn
+end
+
+module CustomInitializers
+  def hash_initializer(*attribute_names)
+    define_method(:initialize) do |*args|
+      data = args.first || {}
+      attribute_names.each do |attribute_name|
+        instance_variable_set "#{attribute_name}", data[attribute_name]
+      end
+    end
+  end
+end
+
+Class.send :include, CustomInitializers
