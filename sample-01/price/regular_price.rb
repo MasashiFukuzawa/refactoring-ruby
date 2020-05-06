a2b71cf1002a67f00218948bc0636eec6a10f8d3
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2020-05-06 15:57:10 +0900

    Type code with State/Strategy

---
+++ sample-01/price/regular_price.rb
@@ -0,0 +1,11 @@
+require './default_price'
+
+class Price::RegularPrice
+  include Price::DefaultPrice
+
+  def charge(days_rented)
+    result = 2
+    result += (days_rented - 2) * 1.5 if days_rented > 2
+    result
+  end
+end
